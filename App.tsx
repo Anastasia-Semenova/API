620622351464c5fefa58b6f31e4188939dfd0107
Anastasia-Semenova <semkach10@gmail.com>
2022-10-09 05:53:41 +0300

    second_window

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, ImageBackground, Button } from 'react-native';
+import { useFonts } from 'expo-font';
+import { StyleSheet, Text, View, Image, ScrollView, ImageBackground, FlatList, SafeAreaView, StatusBar } from 'react-native';
 import image from './assets/image.png';
 import gon1 from './assets/gon1.png';
 import gon2 from './assets/gon2.png';
@@ -11,73 +11,162 @@ import kil3 from './assets/kil3.png';
 import kur1 from './assets/kur3.png';
 import kur2 from './assets/kur2.png';
 import kur3 from './assets/kur1.png';
-import { useFonts } from 'expo-font';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import phone from './assets/phone.png';
+import video from './assets/video.png'
+import ok from './assets/ok.png'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={image} style={styles.image}>
-      <Text style={styles.h1}>Instagram of 2d</Text>
-      <Text style={styles.text}>Gon Freecss</Text>
-      <ScrollView horizontal={true} bounces={false} style={styles.scroll}>
-        <Image style={styles.stretch} source={gon1}/>
-        <Image style={styles.stretch} source={gon2}/>
-        <Image style={styles.stretch} source={gon3}/>
-      </ScrollView>
-      <Text style={styles.text}>Killua Zoldyck</Text>
-      <ScrollView horizontal={true} bounces={false} style={styles.scroll}>
-        <Image style={styles.stretch} source={kil1}/>
-        <Image style={styles.stretch} source={kil2}/>
-        <Image style={styles.stretch} source={kil3}/>
-      </ScrollView>
-      <Text style={styles.text}>Kurapica</Text>
-      <ScrollView horizontal={true} bounces={false} style={styles.scroll}>
-        <Image style={styles.stretch} source={kur1}/>
-        <Image style={styles.stretch} source={kur2}/>
-        <Image style={styles.stretch} source={kur3}/>
-      </ScrollView>
-      <Button title='Please write us'></Button>
-      </ImageBackground>
+const DATA = [
+  {
+    id: '1',
+    image: gon1,
+    title: 'Gon Freeks',
+    data: '12 jan 11:14 am',
+    icon: phone
+  },
+  {
+    id: '2',
+    image: gon2,
+    title: 'Gon Freeks',
+    data: '12 jan 11:14 am',
+    icon: video
+  },
+  {
+    id: '3',
+    image: gon3,
+    title: 'Gon Freeks',
+    data: '12 jan 11:14 am',
+    icon: video
+  },
+  {
+    id: '4',
+    image: kil1,
+    title: 'Killua Zoldic',
+    data: '12 jan 11:14 am',
+    icon: video
+  },
+  {
+    id: '5',
+    image: kil2,
+    title: 'Killua Zoldic',
+    data: '12 jan 11:14 am',
+    icon: phone
+  },
+  {
+    id: '6',
+    image: kur1,
+    title: 'Kurapica',
+    data: '12 jan 11:14 am',
+    icon: phone
+  },
+  {
+    id: '7',
+    image: kur2,
+    title: 'Kurapica',
+    data: '12 jan 11:14 am',
+    icon: video
+  },
+  {
+    id: '8',
+    image: kur3,
+    title: 'Kurapica',
+    data: '12 jan 11:14 am',
+    icon: phone
+  }
+];
+
+const Item = ({ title, image, data, icon }) => (
+  <View style={styles.item}>
+    <View style={styles.left}>
+      <View>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.icondata}>
+          <Image source={ok} style={styles.ok} />
+          <Text style={styles.data}>{data}</Text>
+        </View>
+      </View>
+      <Image source={image} style={styles.image} />
     </View>
+    <Image source={icon} style={styles.icon} />
+  </View>
+);
+
+const App = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} image={item.image} data={item.data} icon={item.icon} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "#f2e8e6",
+  },
+  item: {
     backgroundColor: '#fff',
-    //alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  left: {
+    backgroundColor: '#fff',
+    flexDirection: "row-reverse",
+    flexWrap: "nowrap",
+    justifyContent: "flex-end",
+    flex: 1
+  },
+  title: {
+    fontSize: 16,
+    marginLeft: 10,
+    marginRight: 50,
+    color: '#7bb3ad',
+    fontWeight: '500',
+
   },
   image: {
-    flex: 0,
+    width: 50,
+    height: 50,
     resizeMode: 'cover',
-    justifyContent: 'center',
+    borderRadius: 50,
+    marginLeft: 10
   },
-  stretch: {
-    width: 150,
-    height: 150,
-    resizeMode: 'stretch',
+  data: {
+    fontSize: 14,
+    marginLeft: 3,
+    color: '#b5cbcc',
+    fontWeight: '300',
   },
-  scroll: {
-    width: 150,
-    maxHeight: 150,
+  icon: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
     marginTop: 10,
-    marginBottom: 50,
-    left:130
   },
-  h1: {
-    fontSize:28,
-    fontWeight:'bold',
-    flexDirection:'row',
-    left: 25,
-    marginBottom: 20,
+  ok: {
+    width: 14,
+    height: 14,
+    marginTop: 2,
   },
-  text: {
-    fontSize:20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    left:130
+  icondata: {    
+    marginVertical: 1,
+    marginHorizontal: 10,
+    flexDirection: "row",
+    flexWrap: "nowrap",
   },
 });
+
+export default App;
