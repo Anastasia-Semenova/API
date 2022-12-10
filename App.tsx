1e4e979abd69a02bc5ddac01e307658dc9f72b9f
Anastasia-Semenova <semkach10@gmail.com>
2022-12-10 13:55:17 +0300

    disable button

--- App.tsx
+++ App.tsx
@@ -1,25 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image,} from 'react-native';
-import NumPad from './components/NumPad';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const onPlus = () => setCount(prevCount => prevCount + 1);
-  const onMinus = () => setCount(prevCount => prevCount - 1);
-
+  const [pressedCount, setPressedCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <NumPad plus = {onPlus} minus = {onMinus} state = {count}/>
-    </View>
-  );
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+    <Text style={{ margin: 16 }}>
+    {pressedCount > 0
+    ? `The button was pressed ${pressedCount} times!`
+    : 'The button isn\'t pressed yet'
+    }
+  </Text>
+  <Button
+    title='Press me'
+    onPress={()=>setPressedCount(pressedCount+1)}
+    disabled={true ? pressedCount >= 3 : false}
+  />
+  {pressedCount >= 3 && 
+    <Button
+          title='Able button'
+          onPress={()=>setPressedCount(0)}
+        />
+  }
+ </View>
+ );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-});
