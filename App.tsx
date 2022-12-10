4d67008a0e9a80605c03f40b96cfd6afb488e763
Anastasia-Semenova <semkach10@gmail.com>
2022-12-10 21:01:34 +0300

    boxes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput } from 'react-native';
 import { NavigationContainer } from
-'@react-navigation/native' ;
+  '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginForm from './components/LoginForm';
-import WelcomeScreen from './components/WelcomeScreen';
+import CustomComponents from './components/CustomComponents';
 
-const Stack = createNativeStackNavigator();
+function App() {
+  return (
+    <View style={styles.view}>
+      <CustomComponents />
+    </View>
 
-function App(){
-  return(
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Login' component={LoginForm}/>
-        <Stack.Screen name='Welcome' component={WelcomeScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+      flex: 1,
+      alignContent: 'center',
+      padding: 16,
+  },
+});
+
 export default App;
-
-
